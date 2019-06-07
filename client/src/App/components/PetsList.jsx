5bde303f2725d472f0f4fca9853ea0c65c3160f2
Stephen Guillory <stepheng1492@gmail.com>
2019-06-07 10:21:39 -0500

    rerendering of lost to found works

--- client/src/App/components/PetsList.jsx
+++ client/src/App/components/PetsList.jsx
@@ -1,41 +1,41 @@
-import React from 'react';
-import Axios from 'axios';
-import PetsFeed from './PetsFeed.jsx'
+// import React from 'react';
+// import Axios from 'axios';
+// import PetsFeed from './PetsFeed.jsx'
 
-// Grabs all the pets from state of Pets.jsx, maps through the array and passes each pet to PetsFeed
-class PetsList extends React.Component {
-constructor(props) {
-  super(props)
+// // Grabs all the pets from state of Pets.jsx, maps through the array and passes each pet to PetsFeed
+// class PetsList extends React.Component {
+// constructor(props) {
+//   super(props)
   
- //  this.handleSubmit = this.handleSubmit.bind(this);
+//  //  this.handleSubmit = this.handleSubmit.bind(this);
 
-}
+// }
 
-//  componentDidMount() {
-//    this.getItems()
-//      .then((pets) => {
-//        this.setState({
-//          pet: pets,
-//        });
-//      })
-//      .catch((err) => {
-//        console.error(err);
-//      });
-//  }
+// //  componentDidMount() {
+// //    this.getItems()
+// //      .then((pets) => {
+// //        this.setState({
+// //          pet: pets,
+// //        });
+// //      })
+// //      .catch((err) => {
+// //        console.error(err);
+// //      });
+// //  }
 
-//  handleSubmit() {
-// ..ncjewvhvh
-//  }
+// //  handleSubmit() {
+// // ..ncjewvhvh
+// //  }
 
 
-render() {
-  const { pets } = this.props;
-  return (
-    <div>
-      {pets.map(pet => <PetsFeed pet={pet} key={pet.id} />)}
-    </div>
-  )
-}
-}
+// render() {
+//   const { pets } = this.props;
+//   return (
+//     <div>
+//       {pets.map(pet => <PetsFeed pet={pet} key={pet.id} />)}
+//     </div>
+//   )
+// }
+// }
 
-export default PetsList
+// export default PetsList
